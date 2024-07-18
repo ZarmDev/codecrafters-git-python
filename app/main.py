191c333fa409a424f0c8f5d4cb31e8aff494e157
ZarmDev <darmiyaevdevelop@protonmail.com>
2024-07-18 13:59:33 -0400

    Didn't finish the write-tree implementation, but, now it's close. Only steps left are finding out what to do with the size of the tree-object and making sure everything else right

--- app/main.py
+++ app/main.py
@@ -27,13 +27,79 @@ def hash_file(file_path):
             sha1.update(data)
     return sha1.hexdigest()
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
+def create_tree_object(dir_path):
+    print("tree object directory:", dir_path)
+    '''
+    before Zlib compression:
+      tree <size>\0
+  <mode> <name>\0<20_byte_sha>
+  <mode> <name>\0<20_byte_sha>
+  
+  For files, the valid values are:
+100644 (regular file)
+100755 (executable file)
+120000 (symbolic link)
+For directories, the value is 040000'''
+    total_size = 0
+    tree_end = ""
+    for file in os.listdir(dir_path):
+        print(file)
+        file_path = os.path.join('.', file)
 
-    # Uncomment this block to pass the first stage
+        mode = None
+        # not sure if the exe check works, 
+        # not gonna check for exes or symbolic links right now
+        if os.path.isfile(file) and not ('.exe' in file):
+            mode = "100644"
+            file_size = get_size(os.path.abspath(file_path))
+            total_size += file_size
+        elif os.path.isdir(file):
+            mode = "040000"
+        sha = hash_file(file_path)
+        current_item = f"{mode} {file}\0{sha}"
+        tree_end += current_item
     
+    tree_object = f"tree {total_size}\0"
+    print(tree_object)
+
+# i hope this doesn't brick my pc...
+def write_tree_on_each_file_in_directory(directory_name):
+    print("working directory!", directory_name)
+    # although the name is file, it can be a file or directory
+    for file in os.listdir(directory_name):
+            print(file)
+            # Remember to ignore the .git directory when creating entries in the tree object.
+            if file == '.git':
+                continue
+            sha_hash = None
+            file_path = os.path.join('.', file)
+            # If the entry is a file, create a blob object and record its SHA hash
+            if os.path.isfile(file):
+                with open(file, 'rb') as f:
+                    blobsize = get_size(file_path)
+                    contents = open(file_path, "rb").read()
+                    blob_object = b"blob " + str(blobsize).encode() + b'\0' + contents
+                    # first get the SHA hash of the UNCOMPRESSED contents (including header)
+                    sha_hash = hash_file(file_path)
+                    # print(sha_hash)
+            # If the entry is a directory, recursively create a tree object and record its SHA hash
+            elif os.path.isdir(file):
+                ## Create the tree object ##
+                '''
+                <mode> <name>\0
+                <20_byte_sha><mode> <name>\0
+                <20_byte_sha><mode> <name>\0
+                '''
+                create_tree_object(file_path)
+                
+
+def main():
     command = sys.argv[1]
+    thirdargument = None
+    try:
+        thirdargument = sys.argv[3]
+    except:
+        pass
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -52,10 +118,9 @@ def main():
             Extract the actual "content" from the decompressed data
             Print the content to stdout
         '''
-        blob = sys.argv[3]
         os.chdir('.git/objects')
-        os.chdir(str(blob[:2]))
-        with open(str(blob[2:]), 'rb') as f:
+        os.chdir(str(thirdargument[:2]))
+        with open(str(thirdargument[2:]), 'rb') as f:
             contents = f.read()
             decompressed_data = zlib.decompress(contents)
             filetype = decompressed_data[0:4].decode()
@@ -63,22 +128,21 @@ def main():
             content = decompressed_data[8:].decode()
             print(content, end="")
     elif command == "hash-object":
-        blob = sys.argv[3]
-        blobsize = get_size(blob)
-        contents = open(blob, "rb").read()
-        new_content = b"blob " + str(blobsize).encode() + b'\0' + contents
+        blobsize = get_size(thirdargument)
+        contents = open(thirdargument, "rb").read()
+        blob_object = b"blob " + str(blobsize).encode() + b'\0' + contents
         # print(contents)
         # first get the SHA hash of the UNCOMPRESSED contents (including header)
-        with open(blob, "wb") as f:
-            f.write(new_content)
-        hash = hash_file(blob)
-        print(hash)
+        with open(thirdargument, "wb") as f:
+            f.write(blob_object)
+        sha_hash = hash_file(thirdargument)
+        print(sha_hash)
         # then, compress the contents using zlib
         os.chdir('.git/objects/')
-        os.mkdir(hash[0:2])
-        os.chdir(hash[0:2])
-        with open(hash[2:], "wb") as f:
-            f.write(zlib.compress(new_content))
+        os.mkdir(sha_hash[0:2])
+        os.chdir(sha_hash[0:2])
+        with open(sha_hash[2:], "wb") as f:
+            f.write(zlib.compress(blob_object))
     elif command == 'ls-tree':
         '''Just like blobs, tree objects are stored in the .git/objects directory. 
         If the hash of a tree object is e88f7a929cd70b0274c4ea33b209c97fa845fdbc, 
@@ -89,13 +153,12 @@ def main():
         <mode> <name>\0<20_byte_sha>
         '''
         # First, change the directory to .git/objects 
-        givenHash = sys.argv[3]
         # print(sys.argv)
         # Use the first two characters to access the "e8" part
         os.chdir('.git/objects')
-        os.chdir(str(givenHash[:2]))
+        os.chdir(str(thirdargument[:2]))
         # Use the second part of the hash to access the file with the tree data
-        with open(str(givenHash[2:]), 'rb') as f:
+        with open(str(thirdargument[2:]), 'rb') as f:
             contents = f.read()
             decompressed_data = zlib.decompress(contents)
             # apparently this is the correct way to split the decompressed data
@@ -117,11 +180,6 @@ def main():
             <20_byte_sha><mode> <name>\0
             <20_byte_sha><mode> <name>\0
             '''
-            # we should expect four items
-            # <mode> <name>
-            # \0<20_byte_sha>
-            #<mode> <name>
-            # \0<20_byte_sha>
             otherHalfOfData = firstSplit[1].split(b'\0')
             # do the exception first, and then remove it
             # the exception looks like <mode> <name>\0
@@ -136,19 +194,20 @@ def main():
                 try:
                     mode, name = i[20:].decode().split(' ')
                     # mode, name = i.decode().split(' ')
-                    print(name)
+                    # print(name)
                 except:
                     None
-
-            # mode, name = firstSplit[1].split(b'\0')[0].decode().split(' ')
-            '''
-            content looks like:
-            
-            '''
-            # content = content.decode()
-            # print(content)
-        # Then, access the file using the rest of the hash
-
+    elif command == 'write-tree':
+        # Iterate over the files/directories in the working directory
+        workingdirectory = os.getcwd()
+        write_tree_on_each_file_in_directory(workingdirectory)
+        ## TODO: Once you have all the entries and their SHA hashes, write the tree object to the .git/objects directory
+        # .git/objects/<first 2 chars of sha>/<remaining chars of sha>
+        os.chdir('.git/objects')
+        os.chdir(sha_hash[:2])
+        with open(sha_hash[2:], "wb") as f:
+            pass
+                    
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
